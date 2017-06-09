a313149e5977f1320301c51224851b986641bdb7
yangjitang77 <yangjitang77@gmail.com>
2017-06-09 10:35:30 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,5 @@ belongs_to :user
  validates :billing_address, presence: true
  validates :shipping_name, presence: true
  validates :shipping_address, presence: true
-
+ has_many :product_lists
 end
