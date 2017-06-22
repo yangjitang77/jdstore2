1d76eeb45a28c29551c63fce17f2538b6269fab2
yangjitang77 <yangjitang77@gmail.com>
2017-06-23 00:38:06 +0800

    edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["NSPYIfbOLzx5cTWb9It2GTuzVGrVFKLx7hmGHEEl"]
-  config.qiniu_secret_key    = ENV["x_65_23gWaoJ4NDjFehtCYMKxRydQbjWC8eE5Nyy"]
-  config.qiniu_bucket        = ENV["jdstore-demo2"]
-  config.qiniu_bucket_domain = ENV["oryboviw8.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
